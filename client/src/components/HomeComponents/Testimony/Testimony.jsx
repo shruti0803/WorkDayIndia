07d584d2595b5fd4df8621feb22413f8372d96fe
Atharva Kumar <atharvados12121@gmail.com>
2024-09-07 17:20:46 +0530

    Changes

--- client/src/components/HomeComponents/Testimony/Testimony.jsx
+++ client/src/components/HomeComponents/Testimony/Testimony.jsx
@@ -7,7 +7,7 @@ const Testimony = ({ item }) => {
     <div className="relative inline-block cursor-pointer w-full lg:mx-4 p-4 pt-0">
       <div className="w-full flex items-start lg:items-center flex-col lg:flex-row gap-7 justify-start">
         <div className="w-full lg:w-[40%] relative">
-          <img src={userImg} alt={item.title} className="w-full rounded-md" />
+          <img src={item.uimg} alt={item.title} className="w-full rounded-md" />
           <span className="absolute top-0 w-full h-full flex items-center justify-center text-white">
             <span className="bg-black/50 rounded-full flex items-center w-[60px] h-[60px] justify-center cursor-pointer">
               <BsPlayFill size={59} />
